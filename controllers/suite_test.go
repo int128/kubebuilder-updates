70c0ced4cc19bc2b76826a738c805f0cddd87f32
Hidetake Iwata <int128@gmail.com>
2023-01-04 11:03:40 +0900

    Rename repository

--- controllers/suite_test.go
+++ controllers/suite_test.go
@@ -30,7 +30,7 @@ import (
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
-	webappv1 "int128.github.io/hello-kubebuilder/api/v1"
+	webappv1 "int128.github.io/kubebuilder-updates/api/v1"
 	//+kubebuilder:scaffold:imports
 )
 
