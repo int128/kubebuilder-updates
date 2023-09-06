e503579de20924f51d2d5970346044cf29e43ab7
update-generated-files-action <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-06 07:44:18 +0000

    Generated by GitHub Actions (scaffold / create)
    https://github.com/int128/kubebuilder-updates/actions/runs/6094177871

--- cmd/main.go
+++ cmd/main.go
@@ -30,6 +30,7 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
 
 	webappv1 "int128.github.io/kubebuilder-updates/api/v1"
 	"int128.github.io/kubebuilder-updates/internal/controller"
@@ -67,8 +68,7 @@ func main() {
 
 	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
 		Scheme:                 scheme,
-		MetricsBindAddress:     metricsAddr,
-		Port:                   9443,
+		Metrics:                metricsserver.Options{BindAddress: metricsAddr},
 		HealthProbeBindAddress: probeAddr,
 		LeaderElection:         enableLeaderElection,
 		LeaderElectionID:       "5a35196e.int128.github.io",
