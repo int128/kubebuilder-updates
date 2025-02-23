cd2a4868cff1fd8317d9468e1bcfd3c254954a8d
Hidetake Iwata <int128@gmail.com>
2025-02-23 14:29:39 +0900

    Refactor scaffold script (#180)

--- .github/generate-project.sh
+++ .github/generate-project.sh
@@ -3,36 +3,40 @@ set -eux -o pipefail
 
 : "${GITHUB_BASE_REF}"
 
-git fetch origin --depth=1 "${GITHUB_BASE_REF}:${GITHUB_BASE_REF}"
-kubebuilder_version="$(cat .kubebuilder-version)"
+install_kubebuilder () {
+  local kubebuilder_version="$(cat .kubebuilder-version)"
+  curl -sfL -o kubebuilder "https://github.com/kubernetes-sigs/kubebuilder/releases/download/${kubebuilder_version}/kubebuilder_$(go env GOOS)_$(go env GOARCH)"
+  chmod +x kubebuilder
+  mv kubebuilder /usr/local/bin/
+  kubebuilder version
+}
 
-# Install kubebuilder
-curl -sfL -o kubebuilder "https://github.com/kubernetes-sigs/kubebuilder/releases/download/${kubebuilder_version}/kubebuilder_$(go env GOOS)_$(go env GOARCH)"
-chmod +x kubebuilder
-mv kubebuilder /usr/local/bin/
+scaffold_project () {
+  # Remove the existing files to avoid kubebuilder error
+  rm -vfr \
+    PROJECT \
+    api \
+    cmd \
+    config \
+    internal \
+    bin \
+    hack \
+    test \
+    e2e_test \
+    Makefile \
+    Dockerfile \
+    .golangci.yml \
+    .*ignore \
+    go.*
+
+  kubebuilder init --domain int128.github.io --repo int128.github.io/kubebuilder-updates
+  kubebuilder create api --group webapp --version v1 --kind Guestbook --controller --resource
+}
+
+install_kubebuilder
+scaffold_project
+
 go version
-kubebuilder version
-
-# Remove the existing files to avoid kubebuilder error
-rm -vfr \
-  PROJECT \
-  api \
-  cmd \
-  config \
-  internal \
-  bin \
-  hack \
-  test \
-  e2e_test \
-  Makefile \
-  Dockerfile \
-  .golangci.yml \
-  .*ignore \
-  go.*
-
-# Scaffold the project
-kubebuilder init --domain int128.github.io --repo int128.github.io/kubebuilder-updates
-kubebuilder create api --group webapp --version v1 --kind Guestbook --controller --resource
 make test
 
 # Restore the original files
